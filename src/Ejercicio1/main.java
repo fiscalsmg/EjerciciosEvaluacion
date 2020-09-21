abc30492e467d577b641a04ff4cd9a2aae5c3c72
Fiscal <fiscalguadalupe422@gmail.com>
2020-09-21 18:01:01 -0500

    Ejercicio JavaB0 realizado punto 1

--- src/Ejercicio1/main.java
+++ src/Ejercicio1/main.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        int numero = 127; //Declaramos el número decimal que deseamos pasar a binario.
+        int numero = -10; //Declaramos el número decimal que deseamos pasar a binario.
         String binario = ""; //Variable aux para guardar la cadena de binarios.
-        while (numero > 0) {
-            //Utilización de if simplificado en el cual se verifica si el residuo de la división es 0 en caso de cumplirse se agrega 0 a la var aux.
-            //binario = numero % 2 == 0 ? "0" + binario : "1" + binario;
-            if(numero%2==0){
-                binario="0"+binario;
-            }else {
-                binario="1"+binario;
+        if(numero>0){
+            while (numero > 0) {
+                if (numero % 2 == 0) {
+                    binario = "0" + binario;
+                } else {
+                    binario = "1" + binario;
+                }
+                numero = numero / 2;
             }
-            numero = numero / 2; //Esta condición es la permite salir del bucle.
-        }
-        System.err.print(binario+"\n"); //Imprimes el resultado almacenado en la variable "binario"
-    }
+            System.err.print(binario+"\n");
+        }else {
+            System.out.println("Negativo");
 
+            String bin=Integer.toBinaryString(numero & 0xFF);
+            System.out.println(bin);
+        }
+    }
 }
--- src/JavaB0.java
+++ src/JavaB0.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
 public class JavaB0 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double rango;
+        String num,bin;
+
+        System.out.println("ingresa una cadena de numeros");
+        num=in.nextLine();
+        String[] s= num.split(",");
+        int [] decimal=new int[s.length];
+        for (int i = 0; i< s.length; i++)    {
+           // System.out.print(s[i]+",");
+            decimal[i]=Integer.parseInt(s[i]);
+           // System.out.println(decimal[i]);
+            if(decimal[i]  < 127 && decimal[i] > -128){
+                bin=Integer.toBinaryString(decimal[i] & 0xFF);
+                System.out.print(bin+",");
+            }
+        }
+
+
+        //decimal=Integer.parseInt(s);
+        /*double rango;
 
         rango=Double.parseDouble(JOptionPane.showInputDialog("Introduce una cadena de numeros:"));
         if(rango > 127 && rango < -128){
             System.out.println("Error");
         }else {
             System.out.println("ksddsks");
-        }
+        }*/
 
     }
 
