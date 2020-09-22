4263852a0b75fca44dc0bf9468478cc31747c769
Fiscal <fiscalguadalupe422@gmail.com>
2020-09-22 10:57:55 -0500

    Inicio B1-2 ejercicio con pilas

--- src/JavaB1.java
+++ src/JavaB1.java
@@ -13,12 +13,12 @@ public class JavaB1 {
     static public void triangulo(int n){
         for (int i = 0; i <=n ; i++) {
             for (int j = n-i; j >0 ; j--) {
-                System.out.print(" ");
+                System.out.print(" ");//decremento para los espacios
             }
             for (int j = 0; j <i ; j++) {
-                System.out.print(" *");
+                System.out.print(" *"); //dibuja los *
             }
-            System.out.println("");
+            System.out.println("");//da los saltos respecto a n
         }
     }
     static public void cuadrado(int n){
--- src/JavaB1CadenaEquilibrada.java
+++ src/JavaB1CadenaEquilibrada.java
@@ -4,28 +4,23 @@ import java.util.Stack;
 public class JavaB1CadenaEquilibrada {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Ingresa el valor de la piramide");
+        System.out.println("Ingresa una Cadena");
         String cad= in.nextLine();
-        verifica(cad);
 
+        Stack pila = new Stack();
+        char[] c;
+        for (int i = 0; i < cad.length(); i++) {
+            c=cad.toCharArray();
 
-    }
-    public static boolean verifica(String cadena)  {
-        Stack<String> pila = new Stack<String>();
-        int i = 0;
-        while (i<cadena.length()) {  // Recorremos la expresión carácter a carácter
-            if(cadena.charAt(i)=='(') {pila.push("(");} // Si el paréntesis es de apertura apilamos siempre
-            else if  (cadena.charAt(i)==')') {  // Si el paréntesis es de cierre actuamos según el caso
-                if (!pila.empty()){ pila.pop(); } // Si la pila no está vacía desapilamos
-                else { pila.push(")"); break; } // La pila no puede empezar con un cierre, apilamos y salimos
+            if(c[i]=='('){
+                pila.push(c[i]);
             }
-            i++;
+            if(c[i]== ')'){
+                //pila.push("()");
+                pila.pop();
+            }
         }
-        if(pila.empty()){
-            return true;
-        } else {
-            return false;
-        }
-
+        System.out.println(pila);
     }
+
 }
