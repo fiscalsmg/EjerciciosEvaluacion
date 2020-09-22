22971700bbddf7651a730125f5ba4989f8001af4
Fiscal <fiscalguadalupe422@gmail.com>
2020-09-21 19:31:52 -0500

    Modularizacion del punto 1

--- src/JavaB0.java
+++ src/JavaB0.java
@@ -1,10 +1,10 @@
-import javax.swing.*;
 import java.util.Scanner;
 
 public class JavaB0 {
     private static String num;
     private static String bin;
     private static  String[] s;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
@@ -12,17 +12,19 @@ public class JavaB0 {
         System.out.println("ingresa una cadena de numeros");
         num=in.nextLine();
         s= num.split(",");
+        exercisesOne();
+
+    }
+
+    static public void exercisesOne(){
         int [] decimal=new int[s.length];
         for (int i = 0; i< s.length; i++)    {
-           // System.out.print(s[i]+",");
             decimal[i]=Integer.parseInt(s[i]);
-           // System.out.println(decimal[i]);
             if((decimal[i]  <= 127) && (decimal[i] >= (-128))){
                 bin=Integer.toBinaryString(decimal[i] & 0xFF);
                 System.out.print(bin+",");
             }
         }
-
     }
 
 }
