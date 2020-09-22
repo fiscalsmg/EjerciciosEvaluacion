272c3a8fff06e51b775d12f1ffb4a6cd1ef64963
Fiscal <fiscalguadalupe422@gmail.com>
2020-09-21 21:49:58 -0500

    Java b1 figuras, TRIANGULO

--- src/JavaB1.java
+++ src/JavaB1.java
@@ -5,12 +5,51 @@ public class JavaB1 {
         Scanner in = new Scanner(System.in);
         System.out.println("Ingresa el valor de la piramide");
         int n= in.nextInt();
+
+        //cuadrado(n);
+        //contorno(n);
+        triangulo(n);
+    }
+
+    static public void triangulo(int n){
+        for (int i = 0; i <=n ; i++) {
+            for (int j = n-i; j >0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <i ; j++) {
+                System.out.print(" *");
+            }
+            System.out.println("");
+        }
+    }
+    static public void cuadrado(int n){
         //valida
         for (int i = 0; i <n ; i++) {
             for (int j = 0; j <n ; j++) {
                 System.out.print(" * ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
+    }
+    static public void contorno(int n){
+        //arriba
+        for (int j = 0; j <n ; j++) {
+            System.out.print(" *");
+        }
+        System.out.println("");
+
+        //medio
+        for (int i = 0; i <n-2 ; i++) {
+            System.out.print(" * ");
+            for (int j = 0; j <n-2 ; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("   * ");
+        }
+        //final
+        for (int j = 0; j <n ; j++) {
+            System.out.print(" *");
+        }
+        System.out.println(" ");
     }
 }
