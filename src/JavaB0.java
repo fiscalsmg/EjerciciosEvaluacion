4713ab091b5a30a94a5c4f3c724fa6ac8ff539d6
Fiscal <fiscalguadalupe422@gmail.com>
2020-09-23 11:48:20 -0500

    Creacion de binario a decimal, tomando en cuenta los datos del arreglo de binarios

--- src/JavaB0.java
+++ src/JavaB0.java
@@ -19,22 +19,35 @@ public class JavaB0 {
 
     static public void ejercicio1(){
         int [] binario=new int[marca.length];
+        int num = 0;
+        int [] guarda = new int[marca.length];
+        String cad="";
         for (int i = 0; i< marca.length; i++)    {
             binario[i]=Integer.parseInt(marca[i]);
             if((binario[i]  <= 127) && (binario[i] >= (-128))){
                 bin=Integer.toBinaryString((binario[i] & 0xFF)+ 0x100).substring(1);
-                marcaDecimal=bin.split(",");
-                System.out.print(bin+",");
+               // marcaDecimal=bin.split(",");
+                System.out.print(bin+",");//imprime numeros binarios
+                num=Integer.parseInt(bin,2);//variable num guarda los binarios, se parsea el numero
+                guarda[i]=num;//se guardan los numeros en el arreglo
+               // System.out.print(guarda[i]+",");
+
+                if(num>=0 && num<=255){
+
+                    System.out.println(num);
+                }else{
+                    System.out.println("jaja");
+                }
             }
         }
         System.out.println("\n");
-        for (int i = 0; i < binario.length ; i++) {
-            if((binario[i]  <255) && (binario[i] >= 0)){
-                deci=Integer.toBinaryString((binario[i] & 0xFF)+ 0x100).substring(1);
-                ejercicio2(deci);
+        for (int j = 0; j <guarda.length; j++) {
+            if(guarda[j]>=0 && guarda[j]<=255){
+                //System.out.print(bin+",");127
+                System.out.print(guarda[j]+",");
             }
-
         }
+
     }
 
     static public void ejercicio2(String bin){
